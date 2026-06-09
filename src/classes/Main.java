370969f90d79a1c3ea82b454c1b41c72c7b922ea
bettyhabte <heranity@gmail.com>
2026-06-09 16:42:19 -0500

    comment added

--- src/classes/Main.java
+++ src/classes/Main.java
@@ -2,6 +2,7 @@ package classes;
 
 public class Main {
     public static void main(String[] args){
+        // update performed
         Employee employee1 = new Employee();
         Employee employee2 = new Employee(2, "beth", 10);
         Employee employee3 = new Employee(3, "Alex", 20);
